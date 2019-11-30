f4d0e158987b41da6e44df1e66aea0ebf303447d
scottming <therealscottming@gmail.com>
2019-11-30 11:20:09 +0800

    Remove unused resolver functions

--- lib/real_world_web/resolvers/accounts.ex
+++ lib/real_world_web/resolvers/accounts.ex
@@ -1,34 +1,5 @@
 defmodule RealWorldWeb.Resolvers.Accounts do
-  import Ecto.Query, only: [from: 2]
-
-  alias RealWorld.Repo
-  alias RealWorld.Accounts
-
   def me(_, _args, %{context: %{current_user: user}}) do
     {:ok, user}
-  end
-
-  def is_following?(followee, _, %{context: %{current_user: user}}) do
-    {:ok, Accounts.Users.is_following?(user, followee)}
-  end
-
-  def followers(user, _, _) do
-    query =
-      from q in Accounts.User,
-        join: uf in Accounts.UserFollower,
-        on: uf.user_id == q.id,
-        where: uf.followee_id == ^user.id
-
-    {:ok, query |> Repo.all()}
-  end
-
-  def following(user, _, _) do
-    query =
-      from q in Accounts.User,
-        join: uf in Accounts.UserFollower,
-        on: uf.user_id == q.id,
-        where: uf.user_id == ^user.id
-
-    {:ok, query |> Repo.all()}
   end
 end
--- lib/real_world_web/resolvers/blog.ex
+++ lib/real_world_web/resolvers/blog.ex
@@ -1,5 +1,4 @@
 defmodule RealWorldWeb.Resolvers.Blog do
-  alias RealWorld.Repo
   alias RealWorld.Blog
 
   def articles(_, args, _) do
@@ -12,21 +11,5 @@ defmodule RealWorldWeb.Resolvers.Blog do
 
   def tags(_, _args, _) do
     {:ok, Blog.list_tags()}
-  end
-
-  def favorited_for_article(article, _args, %{context: %{current_user: user}}) do
-    case Blog.find_favorite(article, user) do
-      %Blog.Favorite{} -> {:ok, true}
-      nil -> {:ok, false}
-      other -> other
-    end
-  end
-
-  def author_for_article(article, _, _) do
-    {:ok, article |> Ecto.assoc(:author) |> Repo.one()}
-  end
-
-  def author_for_comment(comment, _, _) do
-    {:ok, comment |> Ecto.assoc(:author) |> Repo.one()}
   end
 end
